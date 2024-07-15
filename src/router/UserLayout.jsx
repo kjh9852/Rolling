f8497ddda80adbde3b095bc28d3e390fe807a6ef
sehwan <ab3433@naver.com>
2024-07-15 14:26:14 +0900

    Fix:router/UserLayout.jsx파일의 import문에서 충돌나는 경로 수정했습니다.

--- src/router/UserLayout.jsx
+++ src/router/UserLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import UserHeader from '../components/post/UserHeader';
+import UserHeader from '../components/Post/UserHeader';
 
 export default function UserLayout() {
   return (
