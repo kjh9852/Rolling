c43eb724eef17c525035031ac57fb0524843749b
sehwan <ab3433@naver.com>
2024-07-15 18:58:14 +0900

    Feat:Section컴포넌트에 가로로가운데정렬줬습니다.

--- src/components/common/Section.jsx
+++ src/components/common/Section.jsx
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 const Section = styled.section`
   max-width: 1200px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export default Section;
