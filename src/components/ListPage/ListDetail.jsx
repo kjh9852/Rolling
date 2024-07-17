39989aedf3a0756c20773356f635b0954715a2c8
sehwan <ab3433@naver.com>
2024-07-17 17:42:24 +0900

    Feat:ListDetail에 디자인먼저 구현해봤습니다.CardList는 일단 임시로 색만넣어놨어요

--- src/components/ListPage/ListDetail.jsx
+++ src/components/ListPage/ListDetail.jsx
@@ -5,16 +5,68 @@ const ListSection = styled(Section)`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-top: 65px;
+`;
+const Container = styled.div`
+  width: 100%;
+  margin-bottom: 64px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
 `;
 
-const TopDiv = styled.div`
-  margin-top: 65px;
+const Title = styled.h2`
+  margin-top: 50px;
+  margin-bottom: 16px;
+  font-size: 24px;
+  line-height: 36px;
+  letter-spacing: -0.01em;
+  font-weight: 700;
+  color: var(--black);
+`;
+
+const CardList = styled.div`
+  width: 1160px;
+  height: 260px;
+  background-color: gray;
+`;
+
+const GoToMakeButton = styled.button`
+  padding: 14px 60px;
+  margin-bottom: 218px;
+  width: 280px;
+  height: 56px;
+  background-color: var(--purple600);
+  font-size: 18px;
+  line-height: 28px;
+  letter-spacing: -0.01em;
+  color: var(--white);
+  border-radius: 12px;
+  transition: background-color 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--purple700);
+  }
+  &:active {
+    background-color: var(--purple800);
+  }
+  &:focus {
+    background-color: var(--purple800);
+  }
 `;
 
 export default function ListDetail() {
   return (
     <ListSection>
-      <TopDiv>내용</TopDiv>
+      <Container>
+        <Title>인기 롤링 페이퍼 🔥</Title>
+        <CardList></CardList>
+        <Title>최근에 만든 롤링 페이퍼 ⭐️️</Title>
+        <CardList></CardList>
+      </Container>
+      <GoToMakeButton>나도 만들어보기</GoToMakeButton>
     </ListSection>
   );
 }
