84373fc1259a6917fdc7276009fe1cf3bedb0cfc
sehwan <ab3433@naver.com>
2024-07-17 19:55:38 +0900

    Feat:api.js에 getREcipients와 getRecipientMessage구현했습니다.

--- src/util/api.js
+++ src/util/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://rolling-api.vercel.app';
+const BASE_URL = 'https://rolling-api.vercel.app/8-8';
 
 export async function getProfileImage() {
   const response = await fetch(`${BASE_URL}/profile-images/`);
@@ -7,3 +7,15 @@ export async function getProfileImage() {
 }
 
 export async function getUserMessage() {}
+
+export async function getRecipients() {
+  const response = await fetch(`${BASE_URL}/recipients`);
+  const body = await response.json();
+  return body.results;
+}
+
+export async function getRecipientMessage(recipientId) {
+  const response = await fetch(`${BASE_URL}/recipients/${recipientId}/message`);
+  const body = await response.json();
+  return body.results;
+}
