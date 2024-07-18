37c18c0993c0f8a68ed5cbf3b466b8d4c45dbbb1
sehwan <ab3433@naver.com>
2024-07-18 11:53:49 +0900

    Fix:PostPage연결이 끊겨있어서 수정했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ const router = createBrowserRouter([
         path: 'list',
         element: <ListPage />,
       },
-      // {
-      //   path: 'post',
-      //   element: <PostPage />,
-      // },
+      {
+        path: 'post',
+        element: <AddPostPage />,
+      },
       {
         path: 'post/:id',
         element: <UserLayout />,
