2ec57e167cbfc38963631ff479f93d4896061aba
kjh9852 <kjh985264@gmail.com>
2024-07-18 14:10:31 +0900

    Feat : backgroundColor, relation 데이터 파일 추가

---
+++ src/util/backgroundColor.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    type: 'beige',
+    background: '--beige200',
+  },
+  {
+    type: 'purple',
+    background: '--purple200',
+  },
+  {
+    type: 'green',
+    background: '--green200',
+  },
+  {
+    type: 'blue',
+    background: '--blue200',
+  },
+];
