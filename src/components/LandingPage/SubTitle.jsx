4d610e0512b7d034bd9cca447e3c33a49c80ba81
sehwan <ab3433@naver.com>
2024-07-18 21:02:35 +0900

    Refactor:SubTitle분리

---
+++ src/components/LandingPage/SubTitle.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const SubTitle = styled.h3`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 28px;
+  letter-spacing: -0.01em;
+  color: var(--gray500);
+`;
+
+export default SubTitle;
