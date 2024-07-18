6013a472f14b9089b4216a12e870c76be43d0451
sehwan <ab3433@naver.com>
2024-07-18 21:01:20 +0900

    Refactor:Title분리

---
+++ src/components/LandingPage/Title.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  margin-top: 16px;
+  margin-bottom: 8px;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 36px;
+  letter-spacing: -0.01em;
+  color: var(--gray900);
+`;
+
+export default Title;
