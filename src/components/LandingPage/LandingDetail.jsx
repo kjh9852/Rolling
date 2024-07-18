a2e4a20124a24920c816cedf4c86d6c1704f8de9
sehwan <ab3433@naver.com>
2024-07-18 20:58:49 +0900

    Refactor:PointBox 분리

--- src/components/LandingPage/LandingDetail.jsx
+++ src/components/LandingPage/LandingDetail.jsx
@@ -2,25 +2,10 @@ import LandingSection from './LandingSection';
 import styled from 'styled-components';
 import { Card1, Card2, CardImg } from './Card';
 import IntroSection from './IntroSection';
+import PointBox from './PointBox';
 import cardImg1 from '../../assets/image/cardImg1.png';
 import cardImg2 from '../../assets/image/cardImg2.png';
 import PrimaryButton from '../common/PrimaryButton';
-
-const PointBox = styled.div`
-  padding: 6px 12px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--purple600);
-  width: 82px;
-  height: 32px;
-  border-radius: 50px;
-  font-size: 14px;
-  color: var(--white);
-  font-weight: 700;
-  letter-spacing: -0.005em;
-  line-height: 20px;
-`;
 
 const Title = styled.h2`
   margin-top: 16px;
