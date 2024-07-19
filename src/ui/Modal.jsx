b872114f5af717596ed3e4bd6558357b2c3040f3
kjh9852 <kjh985264@gmail.com>
2024-07-19 14:04:11 +0900

    Fix : backdrop이외를 눌러도 닫히는 현상

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -32,8 +32,9 @@ export default function Modal({ children, ...props }) {
   };
 
   return (
-    <BackDrop onClick={handleCloseModal}>
+    <>
+      <BackDrop onClick={handleCloseModal} />
       <Dialog open>{children}</Dialog>
-    </BackDrop>
+    </>
   );
 }
