d43e38023457945ab2891e1eec1bfa75cd4ab187
kjh9852 <kjh985264@gmail.com>
2024-07-19 14:05:39 +0900

    Design : 가상 셀렉터 추가, cursor추가

--- src/components/LandingPage/StartButton.jsx
+++ src/components/LandingPage/StartButton.jsx
@@ -6,20 +6,10 @@ const StartButton = styled(PrimaryButton)`
   margin-bottom: 174px;
   line-height: 2.8rem;
   font-size: 1.8rem;
-  transition: all.3s ease;
   span {
     display: flex;
     justify-content: center;
     width: 160px;
-  }
-  &:hover {
-    background-color: var(--purple700);
-  }
-  &:active {
-    background-color: var(--purple800);
-  }
-  &:focus {
-    background-color: var(--purple800);
   }
 `;
 
