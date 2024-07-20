ea6d9545bfcf1ec198089ad6bd7c94fab21f5cbf
최원혁 <mangoj425@gmail.com>
2024-07-20 19:46:16 +0900

    Feat: 모바일 반응형 구현

--- src/components/PostPage/AddPost.jsx
+++ src/components/PostPage/AddPost.jsx
@@ -7,10 +7,15 @@ import styled from 'styled-components';
 import { RecipientMessageForm } from '../../util/api';
 
 const PageWrap = styled.div`
-  padding-top: 60px;
   max-width: 720px;
   margin: 0 auto;
-  margin-top: 60px;
+  margin-top: 95px;
+  box-sizing: border-box;
+
+  @media (max-width: 769px) {
+    margin-top: 26px;
+    padding: 24px 20px;
+  }
 `;
 
 const To = styled.h2`
