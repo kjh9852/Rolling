e2fc2193807043620944fba93ebf96d7025bb878
KimJeongHyeon <kjh985264@gmail.com>
2024-07-22 18:21:49 +0900

    Refactor : 여러 fetch요청 단일 호출 통합

--- src/components/ListPage/MobileListDetail.jsx
+++ src/components/ListPage/MobileListDetail.jsx
@@ -3,68 +3,49 @@ import { useNavigate } from 'react-router-dom';
 import ListSection from './ListSection';
 import Container from './Container';
 import Title from './Title';
-import SlideCardListSection from './SlideCardListSection';
+import CardListSection from './CardListSection';
 import GoToMakeButton from './GoToMakeButton';
 import { getRecipients, getRecipientMessage } from '../../util/api';
 import LeftAlign from './LeftAlign';
-import styled from 'styled-components';
 
-const PaddingBox = styled.div`
-  width: 100%;
-  padding: 0px 20px;
-`;
-
-export default function MobileListDetail() {
-  const [topMessages, setTopMessages] = useState([]);
-  const [bottomMessages, setBottomMessages] = useState([]);
+export default function DesktopListDetail() {
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [currentTopOffset, setCurrentTopOffset] = useState(0);
   const [totalItems, setTotalItems] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const itemsPerPage = 1;
   const navigate = useNavigate();
 
-  useEffect(() => {
-    async function fetchRecipients(setMessages, sortBy) {
-      try {
-        const response = await getRecipients(0, 1000, sortBy); // 모든 데이터를 한 번에 불러옴
-        const recipientsData = response.results;
-        const total = response.count;
-        setTotalItems(total);
-
-        const allMessages = await Promise.all(
-          recipientsData.map(async (recipient) => {
-            const recipientMessages = await getRecipientMessage(recipient.id);
-            return { ...recipient, messages: recipientMessages };
-          })
-        );
-        setMessages(allMessages);
-      } catch (error) {
-        console.error('Failed to fetch recipients and messages:', error);
+  const fetchUser = async () => {
+    setLoading(true);
+    try {
+      const resposne = await fetch(
+        `https://rolling-api.vercel.app/8-8/recipients/?limit=10&offset=${offset}`
+      );
+      const { results, ...data } = await resposne.json();
+      setMessages((prev) => [...prev, ...results]);
+      console.log(results,data);
+      if (data.next !== null) {
+        setOffset((prev) => prev + 10);
+      } else {
+        setLoading(false);
       }
+    } catch (error) {
+      console.log(error);
     }
-
-    fetchRecipients(setTopMessages, 'messageCount');
-  }, []);
+  };
 
   useEffect(() => {
-    async function fetchRecipients(setMessages, sortBy) {
-      try {
-        const response = await getRecipients(0, 1000, sortBy); // 모든 데이터를 한 번에 불러옴
-        const recipientsData = response.results;
-        const total = response.count;
-        setTotalItems(total);
+    fetchUser();
+  }, [offset]);
 
-        const allMessages = await Promise.all(
-          recipientsData.map(async (recipient) => {
-            const recipientMessages = await getRecipientMessage(recipient.id);
-            return { ...recipient, messages: recipientMessages };
-          })
-        );
-        setMessages(allMessages);
-      } catch (error) {
-        console.error('Failed to fetch recipients and messages:', error);
-      }
-    }
-
-    fetchRecipients(setBottomMessages, 'createdAt');
-  }, []);
+  const sortMessages = [...messages].sort(
+    (a, b) => b.messageCount - a.messageCount
+  );
+  const dateSortMessages = [...messages].sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
 
   const handleCardClick = (id) => {
     navigate(`/post/${id}`);
@@ -73,28 +54,22 @@ export default function MobileListDetail() {
   return (
     <ListSection>
       <Container>
-        <LeftAlign>
-          <Title>인기 롤링 페이퍼 🔥</Title>
-          <SlideCardListSection
-            messages={topMessages}
-            totalItems={totalItems}
-            handleCardClick={handleCardClick}
-            sortBy='messageCount'
-          />
-        </LeftAlign>
-        <LeftAlign>
-          <Title>최근에 만든 롤링 페이퍼 ⭐️️</Title>
-          <SlideCardListSection
-            messages={bottomMessages}
-            totalItems={totalItems}
-            handleCardClick={handleCardClick}
-            sortBy='createdAt'
-          />
-        </LeftAlign>
+        <Title>인기 롤링 페이퍼 🔥</Title>
+        <CardListSection
+          loading={loading}
+          messages={sortMessages}
+          currentOffset={currentTopOffset}
+          handleCardClick={handleCardClick}
+        />
+        <Title>최근에 만든 롤링 페이퍼 ⭐️️</Title>
+        <CardListSection
+          loading={loading}
+          messages={dateSortMessages}
+          currentOffset={currentTopOffset}
+          handleCardClick={handleCardClick}
+        />
       </Container>
-      <PaddingBox>
-        <GoToMakeButton to='/post'>나도 만들어보기</GoToMakeButton>
-      </PaddingBox>
+      <GoToMakeButton to='/post'>나도 만들어보기</GoToMakeButton>
     </ListSection>
   );
 }
