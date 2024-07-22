22a1ce593559712e9a85cb8fbedddca9192e912f
KimJeongHyeon <kjh985264@gmail.com>
2024-07-22 18:30:59 +0900

    Style : alignItems 속성 제거

--- src/components/ListPage/Container.jsx
+++ src/components/ListPage/Container.jsx
@@ -5,7 +5,6 @@ const Container = styled.div`
   margin-bottom: 64px;
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
 
   @media (min-width: 769px) and (max-width: 1023px) {
