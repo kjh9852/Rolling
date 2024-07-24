e6a9579014fb5c1267c0d6ffeeee25be2ac127ca
kjh9852 <kjh985264@gmail.com>
2024-07-24 14:42:16 +0900

    Fix : 중복이미지 제거

--- src/util/profileImages.js
+++ src/util/profileImages.js
@@ -4,7 +4,6 @@ export default [
   'https://8team-rolling.netlify.app/profile/man02.jpg',
   'https://8team-rolling.netlify.app/profile/man03.png',
   'https://8team-rolling.netlify.app/profile/girl02.png',
-  'https://8team-rolling.netlify.app/profile/girl03.png',
   'https://8team-rolling.netlify.app/profile/man04.png',
   'https://8team-rolling.netlify.app/profile/man05.png',
   'https://8team-rolling.netlify.app/profile/man06.png',
